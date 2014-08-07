44a2d2204d60f7eda23320590667c0b3fe89076f
Douglas Goodwin <dgoodwin@gmail.com>
2014-08-07 16:11:40 -0700

    rebuild db when DEBUG = True

--- runserver.py
+++ runserver.py
@@ -1,9 +1,35 @@
+from os import rename
 from bikessites import app, auth
 from bikessites.models import Comment
 
-if __name__ == "__main__":
-    auth.User.create_table(fail_silently=True)
+def flushDB():
+    bikeStations = app.config['BIKESTATIONS']
+
+    # let's get rid of the old data.db file
+    dbfile = app.config['DBFILE']
+    delme='/tmp/%s' %(dbfile)
+    try:
+        rename(dbfile,delme)
+    except:
+        # no data.db file here!
+        pass
+    
+    # set up the admin user
+    auth.User.create_table(fail_silently=True)  # make sure table created.
+    admin = auth.User(username='admin', email='', admin=True, active=True)
+    admin.set_password('admin')
+    admin.save()
+
+    # add the default bikestations
     Comment.create_table(fail_silently=True)
+    for bs in bikeStations:
+        # {'lat': '34.12927', 'comment': 'Pilot', 'lon': '-118.14949'}
+        Comment.create(lat=bs['lat'],lon=bs['lon'],comment=bs['comment'],)
+
+
+if __name__ == "__main__":
+    if app.config['DEBUG']:
+        flushDB()
 
     app.run()
 
