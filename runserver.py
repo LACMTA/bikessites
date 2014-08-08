6709e99a067154c780a8adaa89ec6eff1a6ab035
Jung Gatoona <leeju@metro.net>
2014-08-08 16:40:04 -0700

    delete

--- runserver.py
+++
@@ -1,9 +0,0 @@
-from bikessites import app, auth
-from bikessites.models import Comment
-
-if __name__ == "__main__":
-    auth.User.create_table(fail_silently=True)
-    Comment.create_table(fail_silently=True)
-
-    app.run()
-
