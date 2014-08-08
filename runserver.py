dfda79a659d6672be188acc6df587c646a7eb1cb
Jung Gatoona <leeju@metro.net>
2014-08-08 16:50:24 -0700

    Update to add comments and fixed likes

---
+++ runserver.py
@@ -0,0 +1,43 @@
+from os import rename
+import shutil
+from bikessites import app, auth
+from bikessites.models import Comment
+
+def flushDB():
+    bikeStations = app.config['BIKESTATIONS']
+
+    # let's get rid of the old data.db file
+    dbfile = app.config['DBFILE']
+    delme='%s_' %(dbfile)
+    try:
+        shutil.move(dbfile,delme)
+    except:
+        # no data.db file here!
+        pass
+    
+    # set up the admin user
+    auth.User.create_table(fail_silently=True)
+    auth.User.create(username='admin', email='', password='admin', admin=True, active=True)
+
+    # add the default bikestations
+    Comment.create_table(fail_silently=True)
+    for bs in bikeStations:
+        # {'lat':'34.0334882847','lon':'-118.480816291','comment':'Santa Monica','approved':True},
+        Comment.create(
+            lat=bs['lat'],
+            lon=bs['lon'],
+            comment=bs['comment'],
+            approved=bs['approved'],
+            name=bs['name'],
+            email=bs['email'],
+            zipcode=bs['zipcode'],
+            category='metroSelected',
+            )
+
+
+if __name__ == "__main__":
+   # if app.config['DEBUG']:
+     #   flushDB()
+
+    app.run()
+
