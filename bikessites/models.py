6b69449e4f97ee32346b4b457d277ed8d70561aa
Douglas Goodwin <dgoodwin@gmail.com>
2014-08-22 11:47:45 -0700

    use straight rq

--- bikessites/models.py
+++ bikessites/models.py
@@ -6,8 +6,7 @@ from flask_peewee.rest import RestResource, UserAuthentication
 from peewee import FloatField,DateTimeField,TextField,BooleanField,IntegerField,CharField
 import random
 from redis import Redis
-# from rq import Queue
-from flask.ext.rq import get_queue
+from rq import Queue
 
 from bikessites import db, admin, api, api_auth
 from gdocs import _save_gdocs
@@ -37,7 +36,7 @@ class Comment(db.Model):
     def save(self, *args, **kwargs):
         stamp = int( random.randrange(111111111111,999999999999) )
         self.uid = stamp
-        job = get_queue().enqueue( _save_gdocs, self )
+        job = q.enqueue( _save_gdocs, self )
         return super(Comment, self).save(*args, **kwargs)
 
     # def __repr__(self):
