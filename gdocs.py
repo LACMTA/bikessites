c56c64f07c32f7de38a55bec9f2745b500c1b51b
Douglas Goodwin <dgoodwin@gmail.com>
2014-08-22 11:50:07 -0700

    restore gdocs function

--- gdocs.py
+++ gdocs.py
@@ -3,20 +3,21 @@ from config import Configuration
 
 
 def _save_gdocs(obj):
-    # gc = gspread.login(Configuration.GDOCS_USER,Configuration.GDOCS_PASS)
-    # wks = gc.open("bikeshare").sheet1
-    # print "{{{{{{{{{{{ _save_gdocs }}}}}}}}}}}"
-    # print obj
-    # objl = [obj.lat,
-    #     obj.lon,
-    #     obj.comment,
-    #     obj.reply,
-    #     obj.likes,
-    #     obj.name,
-    #     obj.email,
-    #     obj.zipcode,
-    #     obj.category,
-    #     obj.pub_date,
-    #     ]
-    # wks.append_row(objl)
-    print "hey"
+    gc = gspread.login(Configuration.GDOCS_USER,Configuration.GDOCS_PASS)
+    wks = gc.open("bikeshare").sheet1
+    print "{{{{{{{{{{{ _save_gdocs }}}}}}}}}}}"
+    print obj
+    objl = [obj.lat,
+        obj.lon,
+        obj.comment,
+        obj.reply,
+        obj.likes,
+        obj.name,
+        obj.email,
+        obj.zipcode,
+        obj.category,
+        obj.pub_date,
+        ]
+    wks.append_row(objl)
+    
+
