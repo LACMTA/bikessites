8c72eaf25fadf567024e6a3fc5d74558e9a6c212
Douglas Goodwin <dgoodwin@gmail.com>
2014-08-22 15:48:51 -0700

    documentation

--- gdocs.py
+++ gdocs.py
@@ -21,3 +21,31 @@ def _save_gdocs(obj):
     wks.append_row(objl)
     
 
+"""
+The /etc/supervisor/rqworker.ini file will look like this:
+
+[program:rqworker]
+; Point the command to the specific rqworker command you want to run.
+; If you use virtualenv, be sure to point it to
+; /var/www/envs/bikessites/bin/rqworker
+; Also, you probably want to include a settings module to configure this
+; worker.  For more info on that, see http://python-rq.org/docs/workers/
+command=/var/www/envs/bikessites/bin/rqworker
+process_name=%(program_name)s
+
+; If you want to run more than one worker instance, increase this
+numprocs=1
+
+; This is the directory from which RQ is ran. Be sure to point this to the
+; directory where your source code is importable from
+directory=/var/www/envs/bikessites/
+
+; RQ requires the TERM signal to perform a warm shutdown. If RQ does not die
+; within 10 seconds, supervisor will forcefully kill it
+stopsignal=TERM
+
+; These are up to you
+autostart=true
+autorestart=true
+
+"""
