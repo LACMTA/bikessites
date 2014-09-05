978ff01383097074020187378bb487078fb11615
Douglas Goodwin <dgoodwin@gmail.com>
2014-09-05 15:22:58 -0700

    update pub_date on save

--- bikessites/models.py
+++ bikessites/models.py
@@ -15,32 +15,35 @@ from gdocs import _save_gdocs
 redis_conn = Redis('localhost', 6379)
 q = Queue(connection=redis_conn)  # no args implies the default queue
 
-class Comment(db.Model):
-    d = datetime.datetime.now()
-    uid = IntegerField(default=0)
-    lat = FloatField(default=0.0)
-    lon = FloatField(default=0.0)
-    comment = TextField(default='Hi')
-    name = CharField(default='anonymous',max_length=50)
-    email = CharField(default='anonymous@gmail.com',max_length=255)
-    zipcode = CharField(default='90000',max_length=10)
-    reply = TextField(default='')
-    pub_date = DateTimeField(default=d)
-    approved = BooleanField(default=True)
-    likes = IntegerField(default=0)
-    category=CharField(default='userSelected',max_length=16)
-    
-    def __unicode__(self):
-        return '[%f,%f,%s]' % (self.lat, self.lon, self.comment)
-    
-    def save(self, *args, **kwargs):
-        stamp = int( random.randrange(111111111111,999999999999) )
-        self.uid = stamp
-        job = q.enqueue( _save_gdocs, self )
-        return super(Comment, self).save(*args, **kwargs)
 
-    # def __repr__(self):
-    #     return '[%f,%f,%s]' % (self.lat, self.lon, self.comment)
+class Comment(db.Model):
+	d = datetime.datetime.now()
+	uid = IntegerField(default=0)
+	lat = FloatField(default=0.0)
+	lon = FloatField(default=0.0)
+	comment = TextField(default='Hi')
+	name = CharField(default='anonymous',max_length=50)
+	email = CharField(default='anonymous@gmail.com',max_length=255)
+	zipcode = CharField(default='90000',max_length=10)
+	reply = TextField(default='')
+	pub_date = DateTimeField(default=d)
+	approved = BooleanField(default=True)
+	likes = IntegerField(default=0)
+	category=CharField(default='userSelected',max_length=16)
+	
+	def __unicode__(self):
+		return '[%f,%f,%s]' % (self.lat, self.lon, self.comment)
+	
+	def save(self, *args, **kwargs):
+		stamp = int( random.randrange(111111111111,999999999999) )
+		d = datetime.datetime.now()
+		self.uid = stamp
+		self.pub_date = d
+		job = q.enqueue( _save_gdocs, self )
+		return super(Comment, self).save(*args, **kwargs)
+
+	# def __repr__(self):
+	#	 return '[%f,%f,%s]' % (self.lat, self.lon, self.comment)
 
 admin.register(Comment) # register "Comment" with vanilla ModelAdmin
 
@@ -49,8 +52,8 @@ admin.setup()
 
 # Exclude the uid from the resource listing
 class CommentResource(RestResource):
-    paginate_by=1000
-#     exclude = ('uid')
+	paginate_by=1000
+#	 exclude = ('uid')
 
 # register our models so they are exposed via /api/<model>/
 # api.register(Comment, auth=api_auth, allowed_methods=['GET', 'POST', 'PUT'])
